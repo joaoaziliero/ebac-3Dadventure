312abcd248de482467cb3aaf47691b2b7a4d4ab7
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-05-14 21:31:29 -0300

    prepare scripts for player XZ-plane motion

---
+++ Assets/Scripts/Player Motion/XZMotionState.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils.StateMachines.Conventions;
+
+[StateFilter(StateGroupings.PlayerMotion)]
+public class XZMotionState : StateBase
+{
+    private CharacterController _controller;
+    private PlayerMotionSettings _motionSettings;
+
+    private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+        _motionSettings = Resources.Load<PlayerMotionSettings>("PlayerMotionSettings");
+    }
+
+    public override void OnStateEnter()
+    {
+
+    }
+
+    public override void OnStateUpdate()
+    {
+
+    }
+
+    public override void OnStateExit()
+    {
+
+    }
+}
