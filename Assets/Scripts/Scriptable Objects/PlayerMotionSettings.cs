cfcab7a0036de49bda44145d6806ee4d0009de52
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-05-16 12:16:05 -0300

    Update PlayerMotionSettings assets

--- Assets/Scripts/Scriptable Objects/PlayerMotionSettings.cs
+++ Assets/Scripts/Scriptable Objects/PlayerMotionSettings.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 [CreateAssetMenu]
 public class PlayerMotionSettings : ScriptableObject
 {
-
+    public string horizontalAxisName;
+    [Range(-1, 1)] public float horizontalAxisValue;
+    public string verticalAxisName;
+    [Range(-1, 1)] public float verticalAxisValue;
+    public float speed;
 }
