eda7ccb42ed2218837606333d24e2912a992f369
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-06-15 17:20:06 -0300

    add base class for player guns

---
+++ Assets/Scripts/Guns/GunBase.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using R3;
+using System.Linq;
+
+public class GunBase : MonoBehaviour
+{
+    [SerializeField] private GameObject _projectilePrefab;
+    [SerializeField] private int _poolSize;
+    [SerializeField] private float _timeBetweenShots;
+
+    private List<GameObject> _projectilePool;
+
+    private void Awake()
+    {
+        _projectilePool = new List<GameObject>(_poolSize);
+
+        for (int i = 0; i < _poolSize; i++)
+        {
+            _projectilePool.Add(Instantiate(_projectilePrefab));
+            _projectilePool[i].SetActive(false);
+        }
+    }
+
+    public void Shoot(Vector3 initialPosition, Vector3 direction)
+    {
+        var obj = SelectInactiveProjectile();
+        obj.transform.position = initialPosition;
+        obj.transform.forward = direction;
+
+        if (_projectilePool.All(projectile => projectile.activeInHierarchy == false))
+        {
+            obj.SetActive(true);
+        }
+        else
+        {
+            Observable
+                .Timer(TimeSpan.FromSeconds(_timeBetweenShots))
+                .Do(onCompleted: _ => obj.SetActive(true))
+                .Subscribe();
+        }
+    }
+
+    private GameObject SelectInactiveProjectile()
+    {
+        int i = 0;
+        while (_projectilePool[i].activeInHierarchy) { i++; }
+        return _projectilePool[i];
+    }
+}
