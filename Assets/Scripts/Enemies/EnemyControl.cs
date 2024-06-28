1f3e3ead291bbe85bb5e0e9e81ee81aaefd9f047
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-06-28 16:54:04 -0300

    implement enemy control

---
+++ Assets/Scripts/Enemies/EnemyControl.cs
@@ -0,0 +1,52 @@
+using DG.Tweening;
+using R3;
+using R3.Triggers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils.StateMachines.Conventions;
+
+public class EnemyControl : MonoBehaviour
+{
+    [SerializeField] private int _initialLifePoints;
+    [SerializeField] private int _damageByProjectile;
+    [SerializeField] private int _damageMultiplier;
+    [SerializeField] private string _tagForProjectiles;
+    [SerializeField] private Color _colorOnDamage;
+    [SerializeField] private float _colorChangeDuration;
+
+    private StateManager _stateManager;
+    private Collider _collider;
+    private ReactiveProperty<int> _currentLifePoints;
+    private MeshRenderer _meshRenderer;
+
+    private void Awake()
+    {
+        if (_damageMultiplier < 1) _damageMultiplier = 1;
+        _stateManager = GetComponent<StateManager>();
+        _collider = GetComponentInParent<Collider>();
+        _currentLifePoints = new ReactiveProperty<int>(_initialLifePoints);
+        _meshRenderer = GetComponentInParent<MeshRenderer>();
+    }
+
+    private void Start()
+    {
+        _collider
+            .OnTriggerEnterAsObservable()
+            .Where(collision => collision.gameObject.CompareTag(_tagForProjectiles))
+            .Select(collision => _damageByProjectile * _damageMultiplier)
+            .Subscribe(damage => _currentLifePoints.Value -= damage)
+            .AddTo(this);
+
+        _currentLifePoints
+            .Skip(1)
+            .Where(_ => DOTween.IsTweening(_meshRenderer.material) == false)
+            .Subscribe(_ => _meshRenderer.material.DOColor(_colorOnDamage, "_EmissionColor" , _colorChangeDuration).SetLoops(2, LoopType.Yoyo))
+            .AddTo(this);
+
+        _currentLifePoints
+            .Where(value => value <= 0)
+            .Subscribe(_ => _stateManager.ChooseState(StateNames.EnemyDeathState))
+            .AddTo(this);
+    }
+}
