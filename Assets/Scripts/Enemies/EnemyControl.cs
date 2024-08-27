564970d2b464471cb7d798ea6ad76b0aa3a04c58
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-08-26 21:35:51 -0300

    rewrite enemy control from basic health script

--- Assets/Scripts/Enemies/EnemyControl.cs
+++ Assets/Scripts/Enemies/EnemyControl.cs
@@ -1,56 +1,40 @@
-using DG.Tweening;
 using R3;
-using R3.Triggers;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Utils.StateMachines.Conventions;
 
-public class EnemyControl : MonoBehaviour
+public class EnemyControl : HealthBase
 {
-    [SerializeField] private int _initialLifePoints;
-    [SerializeField] private int _damageByProjectile;
-    [SerializeField] private int _damageMultiplier;
-    [SerializeField] private string _tagForProjectiles;
-    [SerializeField] private Color _colorOnDamage;
-    [SerializeField] private float _colorChangeDuration;
-
     private StateManager _stateManager;
-    private Collider _collider;
-    private ReactiveProperty<int> _currentLifePoints;
-    private MeshRenderer _meshRenderer;
     private GameObject _player;
 
-    private void Awake()
+    protected override void Init()
     {
-        if (_damageMultiplier < 1) _damageMultiplier = 1;
+        base.Init();
+        AddToInit();
+    }
+
+    protected override void CheckForDeath(Action onDeath = null)
+    {
+        base.CheckForDeath(() => { _stateManager.ChooseState(StateNames.EnemyDeathState); });
+    }
+
+    protected override void ManageDamage()
+    {
+        base.ManageDamage();
+        LookAtPlayer();
+    }
+
+    private void AddToInit()
+    {
         _stateManager = GetComponent<StateManager>();
-        _collider = GetComponentInParent<Collider>();
-        _currentLifePoints = new ReactiveProperty<int>(_initialLifePoints);
-        _meshRenderer = GetComponentInParent<MeshRenderer>();
         _player = GameObject.FindGameObjectWithTag("Player");
     }
 
-    private void Start()
+    private void LookAtPlayer()
     {
-        _collider
-            .OnTriggerEnterAsObservable()
-            .Where(collision => collision.gameObject.CompareTag(_tagForProjectiles))
-            .Select(collision => _damageByProjectile * _damageMultiplier)
-            .Subscribe(damage => _currentLifePoints.Value -= damage)
-            .AddTo(this);
-
-        _currentLifePoints
-            .Skip(1)
-            .Where(_ => DOTween.IsTweening(_meshRenderer.material) == false)
-            .Subscribe(_ => _meshRenderer.material.DOColor(_colorOnDamage, "_EmissionColor" , _colorChangeDuration).SetLoops(2, LoopType.Yoyo))
-            .AddTo(this);
-
-        _currentLifePoints
-            .Where(value => value <= 0)
-            .Subscribe(_ => _stateManager.ChooseState(StateNames.EnemyDeathState))
-            .AddTo(this);
-
         Observable
             .EveryUpdate()
             .Subscribe(_ => { transform.parent.LookAt(_player.transform); transform.parent.Rotate(0, 180, 0); })
