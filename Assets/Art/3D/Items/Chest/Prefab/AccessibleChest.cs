ff9f1bbb5730794e017a7927b216e082f00ab835
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-10-31 14:51:24 -0300

    create accessible and destructible chests

--- Assets/Art/3D/Items/Chest/Prefab/AccessibleChest.cs
+++ Assets/Art/3D/Items/Chest/Prefab/AccessibleChest.cs
@@ -1,27 +1,76 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.Networking;
+using R3;
 
 public class AccessibleChest : ChestBase
 {
     public GameObject chestLid;
-    public float lidRotationAngle = 90;
+    public GameObject prefabToSpawn;
+    public int prefabCopies;
+    public float lidRotationAngle = -90;
     public float lidRotationPeriod;
+    public string playerTag = "Player";
+
+    private float _angleOfRest;
+    private float _eulerY;
+    private float _eulerZ;
+    private bool _hasSpawned = false;
+
+    private void Awake()
+    {
+        _angleOfRest = chestLid.transform.rotation.eulerAngles.x;
+        _eulerY = chestLid.transform.rotation.eulerAngles.y;
+        _eulerZ = chestLid.transform.rotation.eulerAngles.z;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            DOTween.Kill(chestLid.transform);
+            AwardChestContent();
+        }
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag(playerTag))
+        {
+            DOTween.Kill(chestLid.transform);
+            DeactivateChest();
+        }
+    }
+
+    public void SpawnChestContent()
+    {
+        if (_hasSpawned == false)
+        {
+            for (int i = 0; i < prefabCopies; i++)
+            {
+                Instantiate(prefabToSpawn, transform).transform.localPosition.Set(0, 0, 0);
+            }
+
+            _hasSpawned = true;
+        }
+    }
 
     public override void AwardChestContent()
     {
-        RotateLid(lidRotationAngle, lidRotationPeriod);
+        RotateLid(_angleOfRest + lidRotationAngle, lidRotationPeriod);
+        Observable.Timer(TimeSpan.FromSeconds(1)).Do(onCompleted: _ => SpawnChestContent()).Subscribe();
     }
 
     public override void DeactivateChest()
     {
-        RotateLid(-lidRotationAngle, lidRotationPeriod);
-
+        RotateLid(_angleOfRest, lidRotationPeriod);
     }
 
     public void RotateLid(float angle, float period)
     {
-        chestLid.transform.DORotate(new Vector3(angle, 0, 0), period);
+        chestLid.transform.DORotate(new Vector3(angle, _eulerY, _eulerZ), period);
     }
 }
---
+++ Assets/Art/3D/Items/Chest/Prefab/DestructibleChest.cs
@@ -0,0 +1,50 @@
+using R3;
+using R3.Triggers;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DestructibleChest : ChestBase
+{
+    public Color albedoColor = Color.white;
+    public string projectileTag = "Projectile";
+    public int shotsToDestroy;
+    public List<GameObject> chestContents;
+
+    private SphereCollider _trigger;
+    private List<MeshRenderer> _rendererComponents;
+
+    public void Awake()
+    {
+        if (shotsToDestroy <= 0) shotsToDestroy = 1;
+        _trigger = GetComponent<SphereCollider>();
+        _rendererComponents = GetComponentsInChildren<MeshRenderer>().ToList();
+        _rendererComponents.ForEach(mr => mr.material.color = albedoColor);
+    }
+
+    public void Start()
+    {
+        _trigger
+            .OnTriggerEnterAsObservable()
+            .Where(collider => collider.gameObject.CompareTag(projectileTag))
+            .Select(collision => 1)
+            .Scan(1, (currentSum, collisionCounter) => currentSum + collisionCounter)
+            .Where(sum => sum == shotsToDestroy)
+            .Subscribe(_ => AwardChestContent())
+            .AddTo(this);
+    }
+
+    public override void AwardChestContent()
+    {
+        chestContents.ForEach(obj => obj.transform.SetParent(null, true));
+        DeactivateChest();
+    }
+
+    public override void DeactivateChest()
+    {
+        Destroy(gameObject, 1);
+    }
+}
