6ad5e8998214236976801dc10f95e03a27770748
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-11-09 17:22:38 -0300

    add and adapt trail rendering to projectiles

--- Assets/Scripts/Guns/Projectile.cs
+++ Assets/Scripts/Guns/Projectile.cs
@@ -13,8 +13,25 @@ public class Projectile : MonoBehaviour
     [SerializeField] private string _tagForChests = "Chest";
     [SerializeField] private bool _flipTravelDirection = false;
 
+    private TrailRenderer _trail;
+
+    private void Awake()
+    {
+        _trail = GetComponent<TrailRenderer>();
+    }
+
     private void OnEnable()
     {
+        Observable
+            .Timer(TimeSpan.FromSeconds(0.25))
+            .Do(onCompleted: _ => _trail.emitting = true)
+            .Subscribe();
+
+        Observable
+            .Timer(TimeSpan.FromSeconds(_timeToDeactivate - 1))
+            .Do(onCompleted: _ => _trail.emitting = false)
+            .Subscribe();
+
         Observable
             .Timer(TimeSpan.FromSeconds(_timeToDeactivate))
             .Do(onCompleted: _ => gameObject.SetActive(false))
@@ -26,6 +43,7 @@ public class Projectile : MonoBehaviour
             .Subscribe(_ => gameObject.SetActive(false))
             .AddTo(this);
     }
+
     void Update()
     {
         if (_flipTravelDirection)
