435d60503a172d38803349970c55026665e9b8f5
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-11-25 22:48:37 -0300

    add functions to user interface buttons

---
+++ Assets/Scripts/UI/ButtonFunctions.cs
@@ -0,0 +1,44 @@
+using R3;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonFunctions : MonoBehaviour
+{
+    public string saveFile = "PlayerData.json";
+    private SaveManager _saveManager = null;
+
+    private void Awake()
+    {
+        saveFile = Path.Combine(Application.persistentDataPath, saveFile);
+    }
+
+    private void Update()
+    {
+        if (_saveManager == null && SceneManager.GetActiveScene().buildIndex > 0)
+        {
+            _saveManager = GameObject.FindGameObjectWithTag("Player").GetComponent<SaveManager>();
+        }
+    }
+
+    public void SaveGame()
+    {
+        var latestCheckpoint = File.Exists(saveFile) ?
+            JsonUtility.FromJson<PlayerSaveData>(File.ReadAllText(saveFile)).latestCheckpoint : 0;
+
+        _saveManager.Save(latestCheckpoint);
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        Observable.Timer(TimeSpan.FromSeconds(2)).Subscribe(_ => SceneManager.LoadScene(sceneIndex)).AddTo(this);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
