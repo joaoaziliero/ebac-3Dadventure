2c3c026fe077374e670aec202851b902f7258cef
joaoaziliero <146762748+joaoaziliero@users.noreply.github.com>
2024-11-25 21:40:58 -0300

    include user interface buttons

---
+++ Assets/Scripts/UI/ButtonDisplayManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ButtonDisplayManager : MonoBehaviour
+{
+    //Lista de referências aos botões presentes na cena.
+    public List<GameObject> buttonReferences;
+
+    [Header("Animation Settings")]
+    // duration: duração da animação para cada botão;
+    public float duration;
+    // delay: duração relativa ao intervalo entre a animação de um botão e a do próximo.
+    public float delay;
+
+    private void Awake()
+    {
+        foreach(var button in buttonReferences)
+        {
+            button.SetActive(true);
+        }
+    }
+
+    private void OnEnable()
+    {
+        // Com este loop, cada botão é animado passando de dimensões nulas
+        // até seu tamanho máximo, e a animação do próximo botão fica mais lenta
+        // em relação ao botão anterior.
+        foreach(var button in buttonReferences)
+        {
+            button.transform
+                .DOScale(0, duration)
+                .From()
+                .SetDelay(buttonReferences.IndexOf(button) * delay);
+        }
+    }
+}
